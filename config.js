168acc0e46c2ab7e81ca93b0e4f93d65e08b71a7
ßingen <bingen@aragon.one>
2020-08-05 14:33:54 +0200

    Whitelist pools
    We only want to use a restricted set of pools (initially only one)
    involving ANT.

--- config.js
+++ config.js
@@ -3,4 +3,5 @@ module.exports = {
     //node: "ws://localhost:8545",
     //node: "wss://mainnet.eth.aragon.network:8546",
     node: 'https://mainnet.eth.aragon.network/ws',
+    pools: ['0x2cf9106faf2c5c8713035d40df655fb1b9b0f9b9'],
 };
