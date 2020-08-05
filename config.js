a523ececd338fada3e792bc956d7b3396a5feeef
ßingen <bingen@aragon.one>
2020-08-05 17:36:16 +0200

    Move week and block params to config/utils
    Also, rename week to period.

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
+const BigNumber = require('bignumber.js');
+
 module.exports = {
     //node: "ws://localhost:8546",
     //node: "ws://localhost:8545",
     //node: "wss://mainnet.eth.aragon.network:8546",
     node: 'https://mainnet.eth.aragon.network/ws',
     pools: ['0x2cf9106faf2c5c8713035d40df655fb1b9b0f9b9'],
+    periodBlockDelimiters: [10599000, 10599240, 10599480],
+    blocksPerSnapshot: 256,
+    antPerPeriod: new BigNumber(12500), // ~50k per month
 };
